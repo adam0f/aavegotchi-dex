35b652ac1e8f22342a684d0ff7c3f4aecf4169d1
adam0f <adam00ff00@gmail.com>
2021-08-17 16:34:11 -0400

    done up to web3

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { Gotchi, QueryResponse } from './types';
+import { request } from 'graphql-request';
+import { GotchiListing } from './components/GotchiListing';
+import { SelectedGotchi } from './components/SelectedGotchi';
 import './App.css';
 
+const uri = 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-core-matic';
+
+
 function App() {
+  const [ gotchis, setGotchis ] = useState<Array<Gotchi>>([]); 
+  const [ selectedGotchi, setSelectedGotchi ] = useState<number>(0);
+  
+  const fetchGotchis = async () => {
+    const query = `
+    {
+      aavegotchis(first: 100, orderBy: gotchiId) {
+        id
+        name
+        collateral
+        withSetsNumericTraits
+      }
+    }
+    `
+    const response = await request<QueryResponse>(uri, query);
+    setGotchis(response.aavegotchis)
+  }
+
+  useEffect(() => {
+    fetchGotchis();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="selected-container">
+          {gotchis.length > 0 && (
+            <SelectedGotchi
+              name={gotchis[selectedGotchi].name} 
+              traits={gotchis[selectedGotchi].withSetsNumericTraits}
+            />
+          )}  
+        </div>
+        <div className="gotchi-list">
+          {
+            gotchis.map((gotchi, i) => (
+              <GotchiListing
+                key={gotchi.id}
+                id={gotchi.id}
+                name={gotchi.name}
+                collateralColor="black"
+                selectGotchi={() => setSelectedGotchi(i)}
+                selected={i === selectedGotchi}
+              />
+            ))
+          }  
+        </div>
+      </div>
     </div>
-  );
+  )
 }
+
+
 
 export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+
+export interface Gotchi {
+    collateral: string;
+    id: string;
+    name: string;
+    withSetsNumericTraits: Array<Number>;
+   }
+   
+   export interface QueryResponse {
+    aavegotchis: Array<Gotchi>
+   }
